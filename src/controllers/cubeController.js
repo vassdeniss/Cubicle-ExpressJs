09cf742f52a698a2cdd3b4143a842a33b070993d
Denis Vasilev <vassdeniss@gmail.com>
2023-05-23 15:43:45 +0300

    Added cube create post

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -4,4 +4,12 @@ router.get('/create', (req, res) => {
   res.render('create');
 });
 
+router.post('/create', (req, res) => {
+  res.json({
+    test: 'working!',
+  });
+
+  //res.redirect('/');
+});
+
 module.exports = router;
