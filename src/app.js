74facb3e5a315c2d34452bc67adbcd8fbae11b66
Denis Vasilev <vassdeniss@gmail.com>
2023-05-31 07:42:37 +0300

    Added create accessory page

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const connectDb = require('./config/handleDatabase');
 
 const homeController = require('./controllers/homeController');
 const cubeController = require('./controllers/cubeController');
+const accessoryController = require('./controllers/accessoryController');
 
 const app = express();
 
@@ -20,6 +21,7 @@ connectDb()
 
 app.use(homeController);
 app.use('/cubes', cubeController);
+app.use('/accessories', accessoryController);
 app.get('*', (req, res) => {
   res.redirect('/404');
 });
