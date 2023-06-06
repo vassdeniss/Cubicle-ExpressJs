0ebb442c10a3006bf5a1a73cc5430353ac72e6e4
Denis Vasilev <vassdeniss@gmail.com>
2023-06-06 15:08:56 +0300

    Added login page

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const connectDb = require('./config/handleDatabase');
 const homeController = require('./controllers/homeController');
 const cubeController = require('./controllers/cubeController');
 const accessoryController = require('./controllers/accessoryController');
+const userController = require('./controllers/userController');
 
 const app = express();
 
@@ -22,6 +23,7 @@ connectDb()
 app.use(homeController);
 app.use('/cubes', cubeController);
 app.use('/accessories', accessoryController);
+app.use('/users', userController);
 app.get('*', (req, res) => {
   res.redirect('/404');
 });
