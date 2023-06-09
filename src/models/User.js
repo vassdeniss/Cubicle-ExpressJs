e431cd5344b1cc9eeb91bc3ebea9308f175e9670
Denis Vasilev <vassdeniss@gmail.com>
2023-06-09 16:46:57 +0300

    User DB validation

--- src/models/User.js
+++ src/models/User.js
@@ -2,11 +2,19 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  passwordHash: String,
+  username: {
+    type: String,
+    required: true,
+    match: /^[A-Za-z0-9]+$/,
+    minLength: 5,
+  },
+  password: {
+    type: String,
+    match: /^[A-Za-z0-9]+$/,
+    required: true,
+    minLength: 8,
+  },
 });
-
-userSchema.virtual('password');
 
 userSchema.virtual('repeatPassword').set(function (value) {
   if (value !== this.password) {
@@ -15,7 +23,7 @@ userSchema.virtual('repeatPassword').set(function (value) {
 });
 
 userSchema.pre('save', async function () {
-  this.passwordHash = await bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
 const User = mongoose.model('User', userSchema);
