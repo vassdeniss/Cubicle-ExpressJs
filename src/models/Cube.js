14cc021b9a7012fc5511781c4e94273dcb9cfd52
Denis Vasilev <vassdeniss@gmail.com>
2023-06-09 21:22:11 +0300

    Cube validations

--- src/models/Cube.js
+++ src/models/Cube.js
@@ -1,10 +1,27 @@
 const mongoose = require('mongoose');
 
 const cubeSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  imageUrl: String,
-  difficultyLevel: Number,
+  name: {
+    type: String,
+    required: [true, 'Name is required!'],
+    minLength: [5, 'Length must be at least 5 characters!'],
+    match: /^[A-Za-z0-9\s]+$/,
+  },
+  description: {
+    type: String,
+    required: [true, 'Description is required!'],
+    minLength: [20, 'Length must be at least 20 characters!'],
+    match: /^[A-Za-z0-9\s]+$/,
+  },
+  imageUrl: {
+    type: String,
+    required: false,
+    match: /^http:\/\/|^https:\/\//,
+  },
+  difficultyLevel: {
+    type: Number,
+    required: [true, 'Difficulty is required!'],
+  },
   accessories: [
     {
       type: mongoose.Schema.Types.ObjectId,
