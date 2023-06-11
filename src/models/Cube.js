b174cb50a77b71993a4684b44f769bcf89061e1f
Denis Vasilev <vassdeniss@gmail.com>
2023-06-12 00:40:34 +0300

    Fixed validations

--- src/models/Cube.js
+++ src/models/Cube.js
@@ -4,19 +4,28 @@ const cubeSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Name is required!'],
-    minLength: [5, 'Length must be at least 5 characters!'],
-    match: /^[A-Za-z0-9\s]+$/,
+    minLength: [5, 'Name must be at least 5 characters!'],
+    match: [
+      /^[A-Za-z0-9\s]+$/,
+      'Name can only contain alphanumeric characters (and spaces)!',
+    ],
   },
   description: {
     type: String,
     required: [true, 'Description is required!'],
-    minLength: [20, 'Length must be at least 20 characters!'],
-    match: /^[A-Za-z0-9\s]+$/,
+    minLength: [20, 'Description must be at least 20 characters!'],
+    match: [
+      /^[A-Za-z0-9\s]+$/,
+      'Description can only contain alphanumeric characters (and spaces)!',
+    ],
   },
   imageUrl: {
     type: String,
     required: false,
-    match: /^http:\/\/|^https:\/\//,
+    match: [
+      /^http:\/\/|^https:\/\//,
+      'URL must start with either \'http://\' or \'https://\'!',
+    ],
   },
   difficultyLevel: {
     type: Number,
